27d622a80ac086942306ff7d813eeee7164d908e
exseuit <81846450+exseuit@users.noreply.github.com>
2022-08-26 03:49:45 +0300

    Update hello-w.php

--- hello-w.php
+++ hello-w.php
@@ -1,9 +1,69 @@
 <?php
 
-/**
-* This ia my test function! 
-*/
-function hello_world()
+**
+ *  Creat log file and folder 
+ */
+
+function set_logfilename($tz = 'Europe/Kiev')
 {
-  echo "<h1>Hello my World!</h1><h2>This ia my test function at github!</h2>";
+  $curtime = new DateTime('now', new DateTimeZone($tz));
+  $name = $curtime->format('Y-m-d') . '.log';
+  $path = get_stylesheet_directory() . '/log-files';
+  $file = $path . '/' . $name;
+  if (!file_exists($file)) {
+    if (!file_exists($path)) {
+      mkdir($path);
+    }
+    $myfile = fopen($file, "a+");
+    $txt = $curtime->format('Y-M-d') . PHP_EOL;
+    fwrite($myfile, $txt);
+    fclose($myfile);
+  }
+  return  $file;
+}
+
+
+/**
+ * Save the result in the log file
+ */
+
+
+function write_to_logfile(...$args)
+{
+  global $filename;
+  $myfile = fopen($filename, "a+");
+  $content = fread($myfile, filesize($filename));
+  $add_content = '';
+  foreach ($args as $arg) {
+    $add_content .= $arg;
+  }
+  $content = $add_content . PHP_EOL;
+  fwrite($myfile, $content);
+  fclose($myfile);
+  usleep(50);
+}
+
+function product_update_function()
+{
+  $count = 0;
+  if (($handle = fopen(CSVFILE, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+      $product_id = wc_get_product_id_by_sku($data[0]);
+      if ($product_id) {
+        $product = wc_get_product($product_id);
+        $sku = $data[0];
+        $stock_quantity = $data[2];
+        $regular_price = $data[5];
+        if ($stock_quantity != $product->get_stock_quantity() || $regular_price  != $product->get_regular_price()) {
+          $product->set_stock_quantity($stock_quantity);
+          $product->set_regular_price($regular_price);
+          // $product->set_price( $regular_price );
+          $count++;
+          $product->save();
+        }
+      }
+    }
+  }
+  fclose($handle);
+  return $count;
 }
