4de85ac1afc645983ca05e5b2f27e018ba2ca56f
exseuit <81846450+exseuit@users.noreply.github.com>
2022-10-11 08:35:26 +0300

    Update hello-w.php

--- hello-w.php
+++ hello-w.php
@@ -20,29 +20,70 @@ function write_to_logfile(...$args)
   usleep(50);
 }
 
-function product_update_function()
+function product_update()
 {
-  $count = 0;
+  $countupd = 0;
+  $countadd = 0;
   if (($handle = fopen(CSVFILE, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+      $product_sku = $data[0];
+      $product_slug = $data[0];
+      $product_title = $data[1];
+      $stock_quantity = $data[2];
+      $free_stock_quantity = $data[3];
+      $regular_price = ceil($data[6]);
       $product_id = wc_get_product_id_by_sku($data[0]);
       if ($product_id) {
         $product = wc_get_product($product_id);
-        $sku = $data[0];
-        $stock_quantity = $data[3];
-        $regular_price =  ceil( $data[6] );
         if ($stock_quantity != $product->get_stock_quantity() || $regular_price  != $product->get_regular_price()) {
           $product->set_stock_quantity($stock_quantity);
           $product->set_regular_price($regular_price);
           // $product->set_price( $regular_price );
-          $count++;
+          if ($product_sku != $product->get_slug()) {
+            $product->set_slug($product_sku);
+          }
+          if ($product_title != $product->get_name()) {
+            $product->set_name($product_title);
+          }
+          if (!$product->get_manage_stock()) {
+            $product->set_manage_stock(true);
+          }
+          if ($free_stock_quantity != $product->get_meta('_free_stock')) {
+            update_post_meta($product_id, '_free_stock', $free_stock_quantity);
+          }
+          $countupd++;
           $product->save();
         }
+      } elseif (strpos($product_sku, 'SKU') === false) {
+        $product = new WC_Product_Simple();
+        $product->set_name($product_title);
+        $product->set_slug($product_slug);
+        $product->set_regular_price($regular_price);
+        $product->set_stock_quantity($stock_quantity);
+        $product->set_sku($product_sku);
+        $product->set_manage_stock(true);
+        $countadd++;
+        $product->save();
       }
     }
   }
+
   fclose($handle);
-  return $count;
+  return 'Updated ' . $countupd . " / Added " . $countadd  . ' products';
+}
+
+function reset_stock($args)
+{
+  $products = wc_get_products($args);
+  $count = 0;
+  foreach ($products as $product) {
+    $product->set_stock_quantity(0);
+    $product->set_regular_price(0);
+    $product->save();
+    $count++;
+  }
+
+  return 'Updated ' . $count . ' products - reset stock and price';
 }
 
 function close_r_lib()
